b555511435e3174be0bd001c111a43b919752b26
MohamedM7 <mammeri244@gmail.com>
2023-07-14 04:22:43 +0200

    Creating Main Chatting Classes & Methods()

---
+++ src/main/java/com/mohamed/chat/ChatController.java
@@ -0,0 +1,17 @@
+package com.mohamed.chat;
+
+
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+    public ChatMessage SendMessage(@Payload ChatMessage chat_message ){
+
+return null;// just nowww
+    }
+
+
+
+
+}
---
+++ src/main/java/com/mohamed/chat/ChatMessage.java
@@ -0,0 +1,9 @@
+package com.mohamed.chat;
+
+public class ChatMessage {
+    private String message;
+    private String sender;
+    private MessageType message_type;
+
+
+}
