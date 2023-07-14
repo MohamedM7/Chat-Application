24840045aa795872d5aa6440411db3d504ea7d78
MohamedM7 <mammeri244@gmail.com>
2023-07-14 05:39:36 +0200

    SettingUp ChatMessage & AddUser Methods()

--- src/main/java/com/mohamed/chat/ChatController.java
+++ src/main/java/com/mohamed/chat/ChatController.java
@@ -1,14 +1,26 @@
 package com.mohamed.chat;
 
 
+import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ChatController {
+    @MessageMapping("/chat.SendMessage")
+    @SendTo("/topic/public") // each time the message is payload it will be sent to topic/public
     public ChatMessage SendMessage(@Payload ChatMessage chat_message ){
+      return chat_message;
+    }
 
-return null;// just nowww
+    @MessageMapping("/chat.SendMessage")
+    @SendTo("/topic/public")
+    public ChatMessage AddUser(ChatMessage chat_message, SimpMessageHeaderAccessor headerAccessor){
+        headerAccessor.getSessionAttributes().put("username",chat_message.getSender());//Add username in websocket session
+        return chat_message;
+
     }
 
 
--- src/main/java/com/mohamed/chat/ChatMessage.java
+++ src/main/java/com/mohamed/chat/ChatMessage.java
@@ -1,5 +1,11 @@
 package com.mohamed.chat;
+import lombok.*;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class ChatMessage {
     private String message;
     private String sender;
