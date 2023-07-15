e9ad8c843d7e14ead5b741e13061cd6a7be6e4a3
MohamedM7 <mammeri244@gmail.com>
2023-07-15 03:50:09 +0200

    SettingUp the WebSocketDisconnectListener Method()

--- src/main/java/com/mohamed/chat/config/WebSocketListener.java
+++ src/main/java/com/mohamed/chat/config/WebSocketListener.java
@@ -1,8 +1,12 @@
 package com.mohamed.chat.config;
 
+import com.mohamed.chat.ChatMessage;
+import com.mohamed.chat.MessageType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
@@ -10,8 +14,19 @@ import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 @RequiredArgsConstructor
 @Slf4j
 public class WebSocketListener {
+    private final SimpMessageSendingOperations message_template;
     @EventListener
     public void WebSocketDisconnectListener(SessionDisconnectEvent event){
+        StompHeaderAccessor headerAccessor=StompHeaderAccessor.wrap(event.getMessage());// wrapping the event
+        String username= (String) headerAccessor.getSessionAttributes().get("username");
+        if(username !=null){
+            log.info("User Disconnect {}",username);
+             var chatMessage = ChatMessage.builder()  // Chat information's
+                     .message_type(MessageType.LEAVE)
+                     .sender(username)
+                     .build();
+             message_template.convertAndSend("/topic/public",chatMessage); // the channel were everyone pass through
+        }
 
     }
 
