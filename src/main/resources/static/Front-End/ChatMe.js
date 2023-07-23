5b50d114bfbe66b9a6f4f1077fda1ecc10da9fc6
MohamedM7 <mammeri244@gmail.com>
2023-07-23 04:49:47 +0200

    Updating ReceiveMessage & Creating GetColor function

--- src/main/resources/static/Front-End/ChatMe.js
+++ src/main/resources/static/Front-End/ChatMe.js
@@ -6,6 +6,7 @@ var ContactSection = document.querySelector('#Contact-Section');
 
 
 
+
 var stompClient = null;
 var UserName = null;
 
@@ -49,12 +50,15 @@ function SendMessage(e){
             content: MessageInput.value,
             type: 'CHAT'
         };
-        stompClient.send("/app/chat.sendMessage", {}, JSON.stringify(chatMessage));
+        stompClient.send("/app/chat.SendMessage", {}, JSON.stringify(chatMessage));
         MessageInput.value = '';
     }
     e.preventDefault();
 
 }
+
+var messageArea = document.getElementById('MessagesUl');
+
 
 function ReceiveMessage(e){
     
@@ -64,9 +68,56 @@ function ReceiveMessage(e){
     if(message.type === 'JOIN') {
         li.classList.add('event-message');
         message.content = message.sender + ' joined!';
+    }else if (message.type === 'LEAVE') {
+        li.classList.add('event-message');
+        message.content = message.sender + ' left!';
+    } else {
+        li.classList.add('ChatMSG');
+
+        var avatarElement = document.createElement('i');
+        var avatarText = document.createTextNode(message.sender[0]);
+        avatarElement.appendChild(avatarText);
+        avatarElement.style['background-color'] = GetColor(message.sender);
+
+        li.appendChild(avatarElement);
+
+        var usernameElement = document.createElement('span');
+        var usernameText = document.createTextNode(message.sender);
+        usernameElement.appendChild(usernameText);
+        li.appendChild(usernameElement);
     }
 
+    var textElement = document.createElement('p');
+    var messageText = document.createTextNode(message.content);
+    textElement.appendChild(messageText);
+
+    li.appendChild(textElement);
+
+    messageArea.appendChild(li);
+    messageArea.scrollTop = messageArea.scrollHeight;
+
 }
+
+var colors = [
+    '#2196F3', '#32c787', '#00BCD4', '#ff5652',
+    '#ffc107', '#ff85af', '#FF9800', '#39bbb0'
+];
+
+function GetColor(messageSender) {
+    var cpt = 0;
+    for (var i = 0; i < messageSender.length; i++) {
+        cpt = 31 * cpt + messageSender.charCodeAt(i);
+    }
+    var index = Math.abs(cpt % colors.length);
+    return colors[index];
+}
+
+var UserLoginForm = document.querySelector('#UserLginForm');
+var Contact_Form = document.querySelector('#Contact_Form');
+
+
+UserLoginForm.addEventListener('submit', connect, true)
+Contact_Form.addEventListener('submit', SendMessage, true)
 
 function IfError(e){
     connectingElement.textContent = '- PRESS F5 - failed to connect to the WebSocket ';
